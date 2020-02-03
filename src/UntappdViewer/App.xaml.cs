ce7de8277bde9c64f4a942671bd1cea5c49ef3ef
fvasilets <fvasilets@modem.by>
2020-02-03 17:25:48 +0300

    запуск через BootStrapper

--- src/UntappdViewer/App.xaml.cs
+++ src/UntappdViewer/App.xaml.cs
@@ -13,5 +13,11 @@ namespace UntappdViewer
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            BootStrapper bootstrapper = new BootStrapper();
+            bootstrapper.Run();
+        }
     }
 }
---
+++ src/UntappdViewer/BootStrapper.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using Prism.Unity;
+using Unity;
+
+namespace UntappdViewer
+{
+    public class BootStrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return this.Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+            Window window = Shell as Window;
+            if (window != null && Application.Current != null)
+            {
+                Application.Current.MainWindow = window;
+                Application.Current.MainWindow.Show();
+            }
+        }
+    }
+}
