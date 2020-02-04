4b3893953d43070786838d568342290c404ee0d9
fvasilets <fvasilets@modem.by>
2020-02-04 13:42:55 +0300

    корректное имя интерфейса

--- src/UntappdViewer/Interfaces/IClosable.cs
+++ src/UntappdViewer/Interfaces/IClosable.cs
@@ -3,7 +3,7 @@ using System.ComponentModel;
 
 namespace UntappdViewer.Interfaces
 {
-    public interface ICloseable
+    public interface IClosable
     {
         void Closing(object sender, CancelEventArgs e);
     }
