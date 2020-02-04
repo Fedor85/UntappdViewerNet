aa14f7e32879362e249821bc111cebacd5ec9b53
fvasilets <fvasilets@modem.by>
2020-02-04 16:13:55 +0300

    Сделал фон для основного окна !

--- src/UntappdViewer/Properties/Resources.Designer.cs
+++ src/UntappdViewer/Properties/Resources.Designer.cs
@@ -98,6 +98,16 @@ namespace UntappdViewer.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap untappd_bottles {
+            get {
+                object obj = ResourceManager.GetObject("untappd_bottles", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Предупреждение..
         /// </summary>
         public static string Warning {
