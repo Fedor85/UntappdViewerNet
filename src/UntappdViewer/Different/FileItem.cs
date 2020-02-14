e3b3272edb3c5618cc31b96a37ce0dc02c605aae
fvasilets <fvasilets@modem.by>
2020-02-14 15:18:18 +0300

    работа над динамиским списокм последних файлов

---
+++ src/UntappdViewer/Different/FileItem.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace UntappdViewer
+{
+    public class FileItem
+    {
+        public string FileName { get; }
+
+        public string FilePath { get; }
+
+        public FileItem(string filePath)
+        {
+            FilePath = filePath;
+            FileName = Path.GetFileName(filePath);
+        }
+    }
+}
--- src/UntappdViewer/ViewModels/RecentFilesViewModel.cs
+++ src/UntappdViewer/ViewModels/RecentFilesViewModel.cs
@@ -1,7 +1,24 @@
-﻿namespace UntappdViewer.ViewModels
+﻿using System.Collections.Generic;
+using System.Windows.Input;
+using Prism.Commands;
+
+namespace UntappdViewer.ViewModels
 {
     public class RecentFilesViewModel : ActiveAwareBaseModel
     {
+        public ICommand OpenRecentFileCommand { get; }
+
+        public List<FileItem> FileItems { get; set; }
+
+        public RecentFilesViewModel()
+        {
+            OpenRecentFileCommand = new DelegateCommand<FileItem>(OpenRecentFile);
+        }
+
+        private void OpenRecentFile(FileItem entity)
+        {
+
+        }
 
         protected override void Activate()
         {
