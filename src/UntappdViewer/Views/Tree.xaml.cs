d02222ea715f37855ee5c77fdd17af75e547117f
fvasilets <fvasilets@modem.by>
2020-02-14 11:21:03 +0300

    Прокрутка мыши для дерева

--- src/UntappdViewer/Views/Tree.xaml.cs
+++ src/UntappdViewer/Views/Tree.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace UntappdViewer.Views
 {
@@ -10,6 +12,19 @@ namespace UntappdViewer.Views
         public Tree()
         {
             InitializeComponent();
+        }
+
+        private void OnPreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (!e.Handled)
+            {
+                e.Handled = true;
+                MouseWheelEventArgs eventArg = new MouseWheelEventArgs(e.MouseDevice, e.Timestamp, e.Delta);
+                eventArg.RoutedEvent = UIElement.MouseWheelEvent;
+                eventArg.Source = sender;
+                UIElement parent = ((Control)sender).Parent as UIElement;
+                parent.RaiseEvent(eventArg);
+            }
         }
     }
 }
