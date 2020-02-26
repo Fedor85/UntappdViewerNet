f446110b7977c6efa04b77f213301c2f440c9316
fvasilets <fvasilets@modem.by>
2020-02-26 12:56:23 +0300

    лишнее

--- src/UntappdViewer.Infrastructure/FileHelper.cs
+++ src/UntappdViewer.Infrastructure/FileHelper.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
-using System.Xml.Serialization;
 
 namespace UntappdViewer.Infrastructure
 {
