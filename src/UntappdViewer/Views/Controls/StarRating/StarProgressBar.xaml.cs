77b305d6036124c75a0aa100009e5dd209804add
fvasilets <fvasilets@modem.by>
2020-02-28 19:30:07 +0300

    добавил контрол по выводу рейтинга

---
+++ src/UntappdViewer/Views/Controls/StarRating/StarProgressBar.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace UntappdViewer.Views.Controls.StarRating
+{
+    /// <summary>
+    /// Interaction logic for StarProgressBar.xaml
+    /// </summary>
+    public partial class StarProgressBar : UserControl
+    {
+        public Color BackgroundStarColor
+        {
+            set { ((SolidColorBrush)BackgroundStar.Fill).Color = value; }
+        }
+
+        public double BackgroundStarOpacity
+        {
+            set { ((SolidColorBrush)BackgroundStar.Fill).Opacity = value; }
+        }
+
+        public Color ForegroundStarColor
+        {
+            set { ((SolidColorBrush)ForegroundStar.Foreground).Color = value; }
+        }
+
+        public double ForegroundStarOpacity
+        {
+            set { ((SolidColorBrush)ForegroundStar.Foreground).Opacity = value; }
+        }
+
+        public Color BorderStarColor
+        {
+            set { ((SolidColorBrush)BorderStar.Stroke).Color = value; }
+        }
+
+        public double BorderStarOpacity
+        {
+            set { ((SolidColorBrush)BorderStar.Stroke).Opacity = value; }
+        }
+
+        public double BorderStarThickness
+        {
+            set { BorderStar.StrokeThickness = value; }
+        }
+
+        public double Value
+        {
+            set { ForegroundStar.Value = value; }
+        }
+
+        public StarProgressBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
