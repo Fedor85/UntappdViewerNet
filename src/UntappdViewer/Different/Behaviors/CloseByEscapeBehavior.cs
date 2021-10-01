19e32b429eec48e61ce6c53d28a2ce83910ff50d
fvasilets <fvasilets@modem.by>
2021-10-01 14:24:33 +0300

    закрытие диалога по ESC

---
+++ src/UntappdViewer/Different/Behaviors/CloseByEscapeBehavior.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Microsoft.Xaml.Behaviors;
+
+namespace UntappdViewer.Behaviors
+{
+    public class CloseByEscapeBehavior : Behavior<UserControl>
+    {
+        public static readonly DependencyProperty DependencyProperty = DependencyProperty.Register("Window", typeof(Window), typeof(CloseByEscapeBehavior));
+
+        public Window Window
+        {
+            get
+            {
+                return (Window)GetValue(DependencyProperty);
+            }
+            set
+            {
+                SetValue(DependencyProperty, value);
+            }
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.Loaded += AssociatedObjectLoaded;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.Loaded -= AssociatedObjectLoaded;
+            if (Window != null)
+                Window.PreviewKeyDown -= WindowPreviewKeyDown;
+        }
+
+        private void AssociatedObjectLoaded(object sender, RoutedEventArgs e)
+        {
+            Window = Window.GetWindow(AssociatedObject);
+            if (Window != null)
+                Window.PreviewKeyDown += WindowPreviewKeyDown;
+        }
+
+        private void WindowPreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            Window window = sender as Window;
+            if (window != null && e.Key == Key.Escape)
+                window.Close();
+        }
+    }
+}
