4c886b54d8c8800a20f4d391e49ed2db99d74f46
fvasilets <fvasilets@modem.by>
2021-11-08 19:09:49 +0300

    log build

--- build/CompileRelease.cmd
+++ build/CompileRelease.cmd
@@ -2,8 +2,10 @@ SET msbuildpath=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\M
 SET slndir=..\src\UntappdViewer.sln
 SET releasedir=..\src\UntappdViewer\bin\Release
 SET targetdir=UntappdViewer
+SET logfile=MSBuild.Log.txt
 
+Del %logfile%
 Rmdir /S /Q %targetdir%
 Rmdir /S /Q %releasedir%
-"%msbuildpath%"\MSBuild.exe "%slndir%" /property:Configuration=Release > MSBuild.Log.txt
+"%msbuildpath%"\MSBuild.exe "%slndir%" /property:Configuration=Release > %logfile%
 Xcopy /E /I "%releasedir%" %targetdir%
