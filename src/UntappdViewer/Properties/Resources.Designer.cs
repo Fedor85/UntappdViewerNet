a232956a3cb2868e5fd9871545feb10e2bf04b1d
fvasilets <fvasilets@modem.by>
2022-09-05 13:04:28 +0300

    изменил год

--- src/UntappdViewer/Properties/Resources.Designer.cs
+++ src/UntappdViewer/Properties/Resources.Designer.cs
@@ -659,7 +659,7 @@ namespace UntappdViewer.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to (с) 2021.
+        ///   Looks up a localized string similar to (с) 2022.
         /// </summary>
         public static string Signature2 {
             get {
