3044ae382a8d076949cd260c586029bd8e973f83
fvasilets <fvasilets@modem.by>
2022-09-10 19:10:15 +0300

    обновление если значение отличается

--- src/UntappdWebApiClient/Helpers/UpdateBeersHelper.cs
+++ src/UntappdWebApiClient/Helpers/UpdateBeersHelper.cs
@@ -21,19 +21,19 @@ namespace UntappdWebApiClient
 
                 bool isUpdate = false;
 
-                if (webBeer.RatingScore > 0)
+                if (webBeer.RatingScore > 0 && webBeer.RatingScore != beer.GlobalRatingScore)
                 {
                     beer.GlobalRatingScore = webBeer.RatingScore;
                     isUpdate = true;
                 }
 
-                if (webBeer.BeerIbu > 0)
+                if (webBeer.BeerIbu > 0 && webBeer.BeerIbu != beer.IBU)
                 {
                     beer.IBU = webBeer.BeerIbu;
                     isUpdate = true;
                 }
 
-                if (!String.IsNullOrEmpty(webBeer.BeerDescription))
+                if (!String.IsNullOrEmpty(webBeer.BeerDescription) && !webBeer.BeerDescription.Equals(beer.Description))
                 {
                     beer.Description = webBeer.BeerDescription;
                     isUpdate = true;
