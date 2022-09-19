40dd76b8a063c6c666621a0fa55baa6edcf30703
fvasilets <fvasilets@modem.by>
2022-09-19 15:36:29 +0300

    SmartTextBox поддержка Mask

--- src/UntappdViewer/Views/Controls/SmartTextBox.xaml.cs
+++ src/UntappdViewer/Views/Controls/SmartTextBox.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -23,6 +26,8 @@ namespace UntappdViewer.Views.Controls
         private bool passwordMode;
 
         private string text;
+
+        private string mask;
 
         private string hintText;
 
@@ -51,6 +56,12 @@ namespace UntappdViewer.Views.Controls
                 else
                     ImgShowHide.Visibility = Visibility.Collapsed;          
             }
+        }
+
+        public string Mask
+        {
+            get { return mask; }
+            set { mask = value; }
         }
 
         public string TextBinding
@@ -102,7 +113,7 @@ namespace UntappdViewer.Views.Controls
             TextVisiblePasswordBox.GotFocus += TextBoxGotFocus;
 
             TextPasswordBox.LostFocus += TextBoxLostFocus;
-            TextVisiblePasswordBox.LostFocus += TextBoxLostFocus;
+            TextVisiblePasswordBox.LostFocus += TextBoxLostFocus;          
         }
 
         private void HintTextBoxGotFocus(object sender, RoutedEventArgs e)
@@ -139,18 +150,87 @@ namespace UntappdViewer.Views.Controls
 
         private void TextPasswordBoxPasswordChanged(object sender, RoutedEventArgs e)
         {
+            if(TextPasswordBox.Password.Equals(Text))
+                return;
+
+            if (!String.IsNullOrEmpty(TextPasswordBox.Password) && !ChekMaskText(TextPasswordBox.Password))
+            {
+
+                int currentIndex = GetPasswordBoxCurrentIndex();
+                int delta = TextPasswordBox.Password.Length - Text.Length;
+
+                TextPasswordBox.Password = Text;
+
+                int currentCurrentIndex = currentIndex - delta;
+                if(currentCurrentIndex >= 0)
+                    TextPasswordBox.GetType().GetMethod("Select", BindingFlags.Instance | BindingFlags.NonPublic).Invoke(TextPasswordBox, new object[] { currentCurrentIndex, 0 });
+
+                return;
+            }
+
             TextVisiblePasswordBox.Text = TextPasswordBox.Password;
+        }
+
+        private int GetPasswordBoxCurrentIndex()
+        {
+            MethodInfo selectionMethod = TextPasswordBox.GetType().GetMethod("get_Selection", BindingFlags.Instance | BindingFlags.NonPublic);
+            if (selectionMethod == null)
+                return 0;
+
+            object textSelection = selectionMethod.Invoke(TextPasswordBox, null);
+            if (textSelection == null)
+                return 0;
+
+            PropertyInfo propertyPosition = textSelection.GetType().GetProperty("PropertyPosition", BindingFlags.Instance | BindingFlags.NonPublic);
+            if (propertyPosition == null)
+                return 0;
+
+            object passwordTextPosition = propertyPosition.GetValue(textSelection);
+            if (passwordTextPosition == null)
+                return 0;
+
+            PropertyInfo offsetProperty = passwordTextPosition.GetType().GetProperty("Offset", BindingFlags.Instance | BindingFlags.NonPublic);
+            if (offsetProperty == null)
+                return 0;
+
+            object offset = offsetProperty.GetValue(passwordTextPosition);
+            return offset == null ? 0 : (int) offset;
         }
 
         private void VisibleTextChanged(object sender, TextChangedEventArgs e)
         {
+            if (TextVisiblePasswordBox.Text.Equals(Text))
+                return;
+
+            if (e.Changes.Count == 0)
+                return;
+
+            if (!String.IsNullOrEmpty(TextVisiblePasswordBox.Text) && !ChekMaskText(TextVisiblePasswordBox.Text))
+            {
+                int currentIndex = e.Changes.ToList()[0].Offset;
+                TextVisiblePasswordBox.Text = Text;
+                TextVisiblePasswordBox.CaretIndex = currentIndex;
+                return;
+            }
+
             Text = TextVisiblePasswordBox.Text;
             TextBinding = Text;
+
             if (PasswordMode)
                 ImgShowHide.Visibility = !String.IsNullOrEmpty(Text) ? Visibility.Visible : Visibility.Hidden;
 
             HintTextBox.Visibility = String.IsNullOrEmpty(Text) && !String.IsNullOrEmpty(HintTextBox.Text) ? Visibility.Visible : Visibility.Collapsed;
             ClearButton.Visibility = !String.IsNullOrEmpty(Text) ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        private bool ChekMaskText(string text)
+        {
+            if (String.IsNullOrEmpty(Mask))
+                return true;
+
+            Regex regex = new Regex(Mask);
+            MatchCollection matches = regex.Matches(text);
+            return matches.Count == 1 && matches[0].Value.Equals(text);
         }
 
         private void ImgShowHideMouseLeave(object sender, MouseEventArgs e)
