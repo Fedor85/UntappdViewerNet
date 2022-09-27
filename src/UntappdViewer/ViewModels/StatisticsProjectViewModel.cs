e6dd28f1a0f868a523e3c815e33a948d3454e514
fvasilets <fvasilets@modem.by>
2022-09-27 12:51:57 +0300

    Работа над статистикой проекта - форма

--- src/UntappdViewer/ViewModels/StatisticsProjectViewModel.cs
+++ src/UntappdViewer/ViewModels/StatisticsProjectViewModel.cs
@@ -1,11 +1,23 @@
-﻿using Prism.Regions;
+﻿using System.Windows.Input;
+using Prism.Commands;
+using Prism.Modularity;
+using Prism.Regions;
+using UntappdViewer.Modules;
+using UntappdViewer.Views;
 
 namespace UntappdViewer.ViewModels
 {
     public class StatisticsProjectViewModel : RegionManagerBaseModel
     {
-        public StatisticsProjectViewModel(IRegionManager regionManager) : base(regionManager)
+        private IModuleManager moduleManager;
+
+        public ICommand OkButtonCommand { get; }
+
+        public StatisticsProjectViewModel(IRegionManager regionManager, IModuleManager moduleManager) : base(regionManager)
         {
+            this.moduleManager = moduleManager;
+
+            OkButtonCommand = new DelegateCommand(Exit);
         }
 
         protected override void Activate()
@@ -16,6 +28,12 @@ namespace UntappdViewer.ViewModels
         protected override void DeActivate()
         {
             base.DeActivate();
+        }
+
+        private void Exit()
+        {
+            moduleManager.LoadModule(typeof(UntappdModule).Name);
+            ActivateView(RegionNames.MainRegion, typeof(Untappd));
         }
     }
 }
