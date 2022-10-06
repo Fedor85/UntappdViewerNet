42dcf19ac63868123fd0ed922dfb2c1a96bf4d35
fvasilets <fvasilets@modem.by>
2022-10-06 22:17:49 +0300

    Добавил синхронизацию сколинга

---
+++ src/UntappdViewer/Different/Behaviors/SynchronisedScroll/SynchronisedScrollBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Xaml.Behaviors;
+
+namespace UntappdViewer.Behaviors
+{
+    public class SynchronisedScrollBehavior : Behavior<ScrollViewer>
+    {
+
+        public static readonly DependencyProperty DependencyProperty = DependencyProperty.Register("RegisterToken", typeof(SynchronisedScrollToken), typeof(SynchronisedScrollBehavior), null);
+
+
+        public SynchronisedScrollToken RegisterToken
+        {
+            get
+            {
+                return (SynchronisedScrollToken)GetValue(DependencyProperty);
+            }
+            set
+            {
+                SetValue(DependencyProperty, value);
+            }
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            RegisterToken?.Register(AssociatedObject);
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            RegisterToken?.Unregister(AssociatedObject);
+        }
+    }
+}
---
+++ src/UntappdViewer/Different/Behaviors/SynchronisedScroll/SynchronisedScrollToken.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace UntappdViewer.Behaviors
+{
+    public class SynchronisedScrollToken
+    {
+        private List<ScrollViewer> registeredScrolls = new List<ScrollViewer>();
+
+        public void Unregister(ScrollViewer scroll)
+        {
+            scroll.ScrollChanged -= ScrollChanged;
+            registeredScrolls.Remove(scroll);
+        }
+
+        public void Register(ScrollViewer scroll)
+        {
+            scroll.ScrollChanged += ScrollChanged;
+            registeredScrolls.Add(scroll);
+        }
+
+        private void ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            ScrollViewer sendingScroll = sender as ScrollViewer;
+            foreach (ScrollViewer potentialScroll in registeredScrolls)
+            {
+                if (potentialScroll == sendingScroll)
+                    continue;
+
+                if (potentialScroll.VerticalOffset != sendingScroll.VerticalOffset)
+                    potentialScroll.ScrollToVerticalOffset(sendingScroll.VerticalOffset);
+
+                if (potentialScroll.HorizontalOffset != sendingScroll.HorizontalOffset)
+                    potentialScroll.ScrollToHorizontalOffset(sendingScroll.HorizontalOffset);
+            }
+        }
+    }
+}
