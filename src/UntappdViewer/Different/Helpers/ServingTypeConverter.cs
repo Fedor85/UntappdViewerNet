6231547d2428fc7f08ff204c21a0ffb8946b30a3
fvasilets <fvasilets@modem.by>
2022-10-19 18:14:26 +0300

    Добавил вывод изображения ServingTyp в статистику

---
+++ src/UntappdViewer/Different/Helpers/ServingTypeConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using UntappdViewer.Helpers;
+
+namespace UntappdViewer.Different.Helpers
+{
+    public class ServingTypeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null ? ConverterHelper.GetServingTypeImagePath(value.ToString()) : String.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString();
+        }
+    }
+}
