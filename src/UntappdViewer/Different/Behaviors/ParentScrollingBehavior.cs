6e0a0849982c702527e6f723d875aa353c22007a
fvasilets <fvasilets@modem.by>
2023-10-03 19:48:55 +0300

    при алоэжении ScrollViewer в другой ScrollViewer в основном не рабюотала прокрутка

---
+++ src/UntappdViewer/Different/Behaviors/ParentScrollingBehavior.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Xaml.Behaviors;
+
+namespace UntappdViewer.Behaviors
+{
+    public class ParentScrollingBehavior : Behavior<ScrollViewer>
+    {
+        public static readonly DependencyProperty ParentDependencyProperty = DependencyProperty.Register("ParentScrollViewer", typeof(ParentScrollingToken), typeof(ParentScrollingBehavior));
+        public static readonly DependencyProperty ChildDependencyProperty =  DependencyProperty.Register("ChildScrollViewer", typeof(ParentScrollingToken), typeof(ParentScrollingBehavior));
+
+        public ParentScrollingToken ParentScrollViewer
+        {
+            get
+            {
+                return (ParentScrollingToken)GetValue(ParentDependencyProperty);
+            }
+            set
+            {
+                SetValue(ParentDependencyProperty, value);
+            }
+        }
+
+        public ParentScrollingToken ChildScrollViewer
+        {
+            get
+            {
+                return (ParentScrollingToken)GetValue(ChildDependencyProperty);
+            }
+            set
+            {
+                SetValue(ChildDependencyProperty, value);
+            }
+        }
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            ParentScrollViewer?.SetParentScrollViewer(AssociatedObject);
+            ChildScrollViewer?.AddChildScrollViewer(AssociatedObject);
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            ParentScrollViewer?.Clear();
+            ChildScrollViewer?.Clear();
+        }
+    }
+}
---
+++ src/UntappdViewer/Different/Behaviors/ParentScrollingToken.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace UntappdViewer.Behaviors
+{
+    public class ParentScrollingToken
+    {
+        private ScrollViewer ParentScrollViewer;
+
+        private List<ScrollViewer> ChildScrollViewers;
+
+        public ParentScrollingToken()
+        {
+            ChildScrollViewers = new List<ScrollViewer>();
+        }
+
+        public void SetParentScrollViewer(ScrollViewer scrollViewer)
+        {
+            ParentScrollViewer = scrollViewer;
+        }
+
+        public void AddChildScrollViewer(ScrollViewer scrollViewer)
+        {
+            scrollViewer.PreviewMouseWheel += PreviewMouseWheel;
+            ChildScrollViewers.Add(scrollViewer);
+        }
+
+        public void Clear()
+        {
+            foreach (ScrollViewer scrollViewer in ChildScrollViewers)
+                scrollViewer.PreviewMouseWheel -= PreviewMouseWheel;
+
+            ChildScrollViewers.Clear();
+        }
+
+        private void PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (ParentScrollViewer == null)
+                return;
+
+            //Block child PreviewMouseWheel
+            e.Handled = true;
+            MouseWheelEventArgs mouseWheelEventArgs = new MouseWheelEventArgs(e.MouseDevice, e.Timestamp, e.Delta);
+            mouseWheelEventArgs.RoutedEvent = UIElement.MouseWheelEvent;
+            ParentScrollViewer.RaiseEvent(mouseWheelEventArgs);
+        }
+    }
+}
