cab59c3d3e90afee7f7766a6d5d55ce88dee1fae
fvasilets <fvasilets@modem.by>
2023-10-17 20:56:00 +0300

    новый которол для отображения текста через разделитель

---
+++ src/UntappdViewer.UI/Controls/TextBlockJoin.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace UntappdViewer.UI.Controls
+{
+    public class TextBlockJoin: TextBlock
+    {
+        private static readonly DependencyProperty SeparatorProperty = DependencyProperty.Register("Separator", typeof(string), typeof(TextBlockJoin), new PropertyMetadata(", "));
+
+        public string Separator
+        {
+            get { return (string)GetValue(SeparatorProperty); }
+            set { SetValue(SeparatorProperty, value); }
+        }
+
+        public TextBlockJoin():base()
+        {
+            DataContextChanged += TextDataContextChanged;
+        }
+
+        private void TextDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            Inlines.Clear();
+            IList collection = e.NewValue as IList;
+            if (collection == null)
+                return;
+
+            int count = collection.Count;
+            for (int i = 0; i < count; i++)
+            {
+                Inlines.Add(collection[i].ToString());
+                if (i == count - 1)
+                    break;
+
+                Run separator = new Run(Separator);
+                separator.FontWeight = FontWeights.Bold;
+                Inlines.Add(separator);
+            }
+        }
+    }
+}
