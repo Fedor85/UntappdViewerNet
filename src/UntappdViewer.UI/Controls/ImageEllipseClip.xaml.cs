08e09cf1ccbb3c7fd74c1324868d968193757d13
fvasilets <fvasilets@modem.by>
2023-11-05 17:30:40 +0300

    добавил котрол с обрезкой по элипсу

---
+++ src/UntappdViewer.UI/Controls/ImageEllipseClip.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace UntappdViewer.UI.Controls
+{
+    /// <summary>
+    /// Interaction logic for ImageEllipseClip.xaml
+    /// </summary>
+    public partial class ImageEllipseClip : UserControl
+    {
+        private static readonly DependencyProperty ImageSourceProperty = DependencyProperty.Register("ImageSource", typeof(BitmapSource), typeof(ImageEllipseClip));
+
+        public BitmapSource ImageSource
+        {
+            get { return (BitmapSource)GetValue(ImageSourceProperty); }
+            set { SetValue(ImageSourceProperty, value); }
+        }
+
+
+        public ImageEllipseClip()
+        {
+            InitializeComponent();
+            SizeChanged += ImageEllipseClipSizeChanged;
+            UImage.Loaded += ImageLoaded;
+            UImage.SetBinding(Image.SourceProperty, new Binding { Path = new PropertyPath(ImageSourceProperty), Source = this });
+        }
+
+        private void ImageEllipseClipSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            UpdateClipEllipseGeometry();
+        }
+
+        private void ImageLoaded(object sender, RoutedEventArgs e)
+        {
+            UpdateClipEllipseGeometry();
+            UImage.Loaded -= ImageLoaded;
+        }
+
+        private void UpdateClipEllipseGeometry()
+        {
+            ClipEllipseGeometry.Center = new Point(ActualWidth / 2, ActualHeight / 2); ;
+            ClipEllipseGeometry.RadiusX = UImage.ActualWidth / 2;
+            ClipEllipseGeometry.RadiusY = UImage.ActualHeight / 2;
+        }
+    }
+}
