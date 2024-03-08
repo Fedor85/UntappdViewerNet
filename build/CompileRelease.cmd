99dad2078a9e062ad4df961ca89bec7a36345767
fvasilets <fvasilets@modem.by>
2024-03-08 14:56:56 +0300

    подкаректировал файл компиляции релиза

--- build/CompileRelease.cmd
+++ build/CompileRelease.cmd
@@ -1,6 +1,7 @@
 SET msbuildpath=E:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin
 SET slndir=..\src\UntappdViewer.sln
 SET releasedir=..\src\UntappdViewer\bin\Release
+SET targetosversion=net8.0-windows10.0.22621.0
 SET targetdir=UntappdViewer
 SET logfile=MSBuild.Log.txt
 
@@ -8,5 +9,5 @@ Del %logfile%
 Rmdir /S /Q %targetdir%
 Rmdir /S /Q %releasedir%
 "%msbuildpath%"\MSBuild.exe "%slndir%" /property:Configuration=Release > %logfile%
-Xcopy /E /I "%releasedir%" %targetdir%
+Xcopy /E /I "%releasedir%"\"%targetosversion%" %targetdir%
 pause
