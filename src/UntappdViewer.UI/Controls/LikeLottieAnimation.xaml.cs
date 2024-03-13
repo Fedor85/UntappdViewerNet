358324f7255b9b83a52c5a0607b93e9a457db433
fvasilets <fvasilets@modem.by>
2024-03-13 15:02:12 +0300

    Добвал контрол по анимации лаков на базе LottieSharp

---
+++ src/UntappdViewer.UI/Controls/LikeLottieAnimation.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace UntappdViewer.UI.Controls
+{
+    /// <summary>
+    /// Interaction logic for LikeLottieAnimation.xaml
+    /// </summary>
+    public partial class LikeLottieAnimation : UserControl
+    {
+        public LikeLottieAnimation()
+        {
+            InitializeComponent();
+        }
+    }
+}
