8b798d41475ef2cf1ef5c4c799140c21d06691a2
fvasilets <fvasilets@modem.by>
2024-03-14 16:40:59 +0300

    Новый контрол Bubbles

---
+++ src/UntappdViewer.UI/Controls/BubblesLottieAnimation.xaml.cs
@@ -0,0 +1,29 @@
+﻿using LottieSharp.WPF;
+using System;
+using System.Windows.Controls;
+using UntappdViewer.UI.Helpers;
+
+namespace UntappdViewer.UI.Controls
+{
+    /// <summary>
+    /// Interaction logic for BubblesLottieAnimation.xaml
+    /// </summary>
+    public partial class BubblesLottieAnimation : UserControl
+    {
+        public BubblesLottieAnimation()
+        {
+            InitializeComponent();
+            SizeChanged += BubblesLottieAnimationSizeChanged;
+        }
+
+        private void BubblesLottieAnimationSizeChanged(object sender, System.Windows.SizeChangedEventArgs e)
+        {
+            double maxSize = Math.Max(ActualHeight, ActualWidth);
+            foreach (LottieAnimationView lottieAnimation in UIHelper.FindVisualChildren<LottieAnimationView>(MainGrid))
+            {
+                lottieAnimation.Width = maxSize;
+                lottieAnimation.Height = maxSize;
+            }
+        }
+    }
+}
