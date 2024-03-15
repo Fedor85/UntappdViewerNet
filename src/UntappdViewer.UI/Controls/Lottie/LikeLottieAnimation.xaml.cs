28f2ae56d6ea237c803f76e06d35a9757ff48382
fvasilets <fvasilets@modem.by>
2024-03-15 17:18:25 +0300

    Добавил возможность маршрутизации событий между контролами

--- src/UntappdViewer.UI/Controls/Lottie/LikeLottieAnimation.xaml.cs
+++ src/UntappdViewer.UI/Controls/Lottie/LikeLottieAnimation.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using UntappdViewer.UI.Helpers;
 
 namespace UntappdViewer.UI.Controls.Lottie
 {
@@ -7,9 +9,28 @@ namespace UntappdViewer.UI.Controls.Lottie
     /// </summary>
     public partial class LikeLottieAnimation : UserControl
     {
+        public static readonly DependencyProperty RoutedControlProperty = DependencyProperty.Register("RoutedControl", typeof(UIElement), typeof(LikeLottieAnimation), new PropertyMetadata(InitializeRoutedControl));
+
+        public UIElement RoutedControl
+        {
+            get { return (UIElement)GetValue(RoutedControlProperty); }
+            set { SetValue(RoutedControlProperty, value); }
+        }
+
         public LikeLottieAnimation()
         {
             InitializeComponent();
+        }
+
+        private static void InitializeRoutedControl(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+        {
+            UIElement routedControl = e.NewValue as UIElement;
+            if (routedControl == null)
+                return;
+
+            LikeLottieAnimation likeLottieAnimation = dependencyObject as LikeLottieAnimation;
+            HandlerRoutedEvent handlerRoutedEvent = new HandlerRoutedEvent(routedControl);
+            handlerRoutedEvent.Initialize(likeLottieAnimation, "Mouse");
         }
     }
 }
---
+++ src/UntappdViewer.UI/Helpers/HandlerRoutedEvent .cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+
+namespace UntappdViewer.UI.Helpers
+{
+    public class HandlerRoutedEvent(UIElement target)
+    {
+        private readonly MethodInfo genericHandlerMethod = typeof(HandlerRoutedEvent).GetMethod("Handler", BindingFlags.Instance | BindingFlags.NonPublic);
+        
+        public void Initialize(UIElement source, string eventNameContains = null)
+        {
+            IEnumerable<EventInfo> eventInfos = source.GetType().GetEvents();
+            if (!String.IsNullOrEmpty(eventNameContains))
+                eventInfos = eventInfos.Where(item => item.Name.ToLower().Contains(eventNameContains.ToLower()));
+
+            foreach (EventInfo eventInfo in eventInfos)
+            {
+                Delegate handler = GetHandlerFor(eventInfo);
+                eventInfo.AddEventHandler(source, handler);
+            }
+        }
+
+        private void Handler<T>(object? sender, T args)
+        {
+            if (args is RoutedEventArgs routedEventArgs)
+                target.RaiseEvent(routedEventArgs);
+        }
+
+        private Delegate GetHandlerFor(EventInfo eventInfo)
+        {
+            Type eventHandlerType = eventInfo.EventHandlerType;
+            ParameterInfo[] parameterInfos = eventHandlerType.GetMethod("Invoke").GetParameters();
+            if (parameterInfos.Length < 2)
+                throw new ApplicationException("Couldn't get event args from eventInfo.");
+
+            Type? eventArgsType = parameterInfos[1].ParameterType;
+            if (eventArgsType is null)
+                throw new ApplicationException("Couldn't get event args type from eventInfo.");
+
+            MethodInfo handlerMethod = genericHandlerMethod.MakeGenericMethod(eventArgsType);
+            if (handlerMethod is null)
+                throw new ApplicationException("Couldn't get handlerMethod from genericHandlerMethod.");
+
+            return Delegate.CreateDelegate(eventHandlerType, this, handlerMethod);
+        }
+    }
+}
